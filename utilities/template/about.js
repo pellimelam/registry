835e6f7396e8c16af2991e1378722544f410d7b7
pellimelam <pellimelam01@gmail.com>
2026-03-20 18:07:45 +0530

    Update about.js

--- utilities/template/about.js
+++ utilities/template/about.js
@@ -1,1 +1,17 @@
+import { getBaseHTML } from "./baseTemplate.js";
 
+export function generateAbout(data){
+
+const about = data.about || "No description yet.";
+
+const content = `
+<h1>About</h1>
+
+<div class="card">
+<p>${about}</p>
+</div>
+`;
+
+return getBaseHTML("About", content);
+
+}
