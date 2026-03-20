6819aa1cf0a323c5deecbd26d07b621af7797e19
pellimelam <pellimelam01@gmail.com>
2026-03-20 17:33:55 +0530

    Create gallery.js

---
+++ utilities/template/gallery.js
@@ -0,0 +1,19 @@
+import { getBaseHTML } from "./baseTemplate.js";
+
+export function generateGallery(data){
+
+const images = data.gallery || ["","","","",""];
+
+const content = `
+<h1>Gallery</h1>
+
+${images.map(img=>`
+<div class="card">
+${img ? `<img src="${img}">` : "No Image"}
+</div>
+`).join("")}
+`;
+
+return getBaseHTML("Gallery", content);
+
+}
