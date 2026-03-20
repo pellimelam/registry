cb9fe307d90f4d7eb0b013ba604e7acefac5c9ae
pellimelam <pellimelam01@gmail.com>
2026-03-20 18:09:30 +0530

    Update videos.js

--- utilities/template/videos.js
+++ utilities/template/videos.js
@@ -1,1 +1,22 @@
+import { getBaseHTML } from "./baseTemplate.js";
 
+export function generateVideos(data){
+
+const videos = data.videos || ["","","","",""];
+
+const content = `
+<h1>Videos</h1>
+
+${videos.map(v => `
+<div class="card">
+${v 
+? `<iframe src="${v.replace("watch?v=","embed/")}" 
+style="width:100%;height:220px;border:none;border-radius:10px;"></iframe>` 
+: "No Video"}
+</div>
+`).join("")}
+`;
+
+return getBaseHTML("Videos", content);
+
+}
