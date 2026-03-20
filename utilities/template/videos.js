3659f7290c8d639f23e2a1bc5ef9eabbb3a4e684
pellimelam <pellimelam01@gmail.com>
2026-03-20 19:40:30 +0530

    Update videos.js

--- utilities/template/videos.js
+++ utilities/template/videos.js
@@ -2,19 +2,16 @@ import { getBaseHTML } from "./baseTemplate.js";
 
 export function generateVideos(data){
 
-const videos = data.videos || ["","","","",""];
-
 const content = `
-<h1>Videos</h1>
 
-${videos.map(v => `
-<div class="card">
-${v 
-? `<iframe src="${v.replace("watch?v=","embed/")}" 
-style="width:100%;height:220px;border:none;border-radius:10px;"></iframe>` 
-: "No Video"}
+<h2>Videos</h2>
+
+<div class="grid">
+${data.videos.map(v => v ? `
+<iframe src="${v.replace("watch?v=","embed/")}"></iframe>
+` : "").join("")}
 </div>
-`).join("")}
+
 `;
 
 return getBaseHTML("Videos", content);
