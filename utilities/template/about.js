6a8facb1b887fd9543b51455d7d188a57a5e6127
pellimelam <pellimelam01@gmail.com>
2026-03-20 19:40:55 +0530

    Update about.js

--- utilities/template/about.js
+++ utilities/template/about.js
@@ -2,14 +2,14 @@ import { getBaseHTML } from "./baseTemplate.js";
 
 export function generateAbout(data){
 
-const about = data.about || "No description yet.";
-
 const content = `
-<h1>About</h1>
+
+<h2>About</h2>
 
 <div class="card">
-<p>${about}</p>
+<p>${data.about || "No description yet."}</p>
 </div>
+
 `;
 
 return getBaseHTML("About", content);
