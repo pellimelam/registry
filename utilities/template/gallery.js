b9a0a91abceaeae0529d8bbed1a9d5f76b69b92d
pellimelam <pellimelam01@gmail.com>
2026-03-20 19:39:42 +0530

    Update gallery.js

--- utilities/template/gallery.js
+++ utilities/template/gallery.js
@@ -2,16 +2,14 @@ import { getBaseHTML } from "./baseTemplate.js";
 
 export function generateGallery(data){
 
-const images = data.gallery || ["","","","",""];
-
 const content = `
-<h1>Gallery</h1>
 
-${images.map(img => `
-<div class="card">
-${img ? `<img src="${img}">` : "No Image"}
+<h2>Gallery</h2>
+
+<div class="grid">
+${data.gallery.map(img => img ? `<img src="${img}">` : "").join("")}
 </div>
-`).join("")}
+
 `;
 
 return getBaseHTML("Gallery", content);
