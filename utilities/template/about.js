fe2389d63357a91a4c74984218817285cb4fb480
pellimelam <pellimelam01@gmail.com>
2026-03-22 18:10:28 +0530

    Update about.js

--- utilities/template/about.js
+++ utilities/template/about.js
@@ -1,5 +1,18 @@
 import { layout } from "./baseTemplate.js";
 
+function formatName(str){
+return str.replace(/-/g," ").replace(/\b\w/g,c=>c.toUpperCase());
+}
+
+function extractPincode(village){
+const match = village.match(/\d{6}$/);
+return match ? match[0] : "";
+}
+
+function getVillageName(str){
+return str.split("-")[0].replace(/\b\w/g,c=>c.toUpperCase());
+}
+
 export function generateAbout(data){
 
 const content = `
@@ -7,17 +20,64 @@ const content = `
 <h2>About</h2>
 
 <div class="card">
-<p>
-Hello, I am <b>${data.firstName} ${data.lastName}</b>, a professional 
-<b>${data.instrument}</b> artist from 
-<b>${data.location.village}</b>, ${data.location.district}.
 
-I perform in weddings, cultural events, and traditional ceremonies.
+<p style="line-height:1.6;color:#cbd5f5;">
+
+Hello, I am <b>${data.firstName} ${data.lastName}</b>, a dedicated and professional 
+<b>${data.instrument}</b> artist based in 
+<b>
+${getVillageName(data.location.village)},
+${formatName(data.location.subdistrict)},
+${formatName(data.location.district)},
+${formatName(data.location.state)}
+</b>
+
 </p>
+
+<p style="line-height:1.6;color:#cbd5f5;">
+
+I specialize in performing at weddings, cultural events, traditional ceremonies, and special occasions, bringing authentic musical experience to every event.
+
+</p>
+
+<p style="line-height:1.6;color:#cbd5f5;">
+
+With passion and experience in my field, I aim to deliver high-quality performances that create memorable moments for every audience.
+
+</p>
+
+</div>
+
+
+<div class="card">
+
+<h3>Services</h3>
+
+<ul style="color:#cbd5f5;line-height:1.8;padding-left:18px;">
+<li>Wedding Performances</li>
+<li>Cultural Events</li>
+<li>Temple Programs</li>
+<li>Private Functions</li>
+</ul>
+
+</div>
+
+
+<div class="card">
+
+<h3>Location</h3>
+
+<p style="color:#cbd5f5;">
+${getVillageName(data.location.village)} Village<br>
+${formatName(data.location.subdistrict)} Mandal<br>
+${formatName(data.location.district)} District<br>
+${formatName(data.location.state)}<br>
+India - ${extractPincode(data.location.village)}
+</p>
+
 </div>
 
 `;
 
 return layout(data, content);
-
 }
